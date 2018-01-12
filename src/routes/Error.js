6e31a68d7dd7b686443a1f4c134a9bd79c9b2a8f
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-12 02:03:36 +0200

    Add a login form

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,18 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
+import WP from '../lib/WP'
+import { dumpObject } from '../lib/helpers'
 
 // import { ResolverError } from './Resolver'
 
+const login = (e) => {
+  const form = e.target
+  WP.authenticate(form.elements[0].value, form.elements[1].value)
+    .then(r => {
+      console.log(r)
+      if (r && r.token) {
+        window.location.reload()
+      }
+
+      form.reset()
+    }).catch(console.error)
+
+  e.preventDefault();
+}
+
 const Error = (props) => {
   const { name, message } = props.error
-  console.log(props)
 
   return (
     <Layout {...props} sidebar={false}>
       <h1>{name}</h1>
       <p>{message}</p>
+      {dumpObject(props)}
+
+    {name === 'Forbidden' ? (
+      <form onSubmit={login}>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <input type="submit" />
+      </form>
+    ) : false}
     </Layout>
   )
+}
+
+Error.propTypes = {
+  error: PropTypes.object.isRequired,
+}
+
+Error.defaultProps = {
+  error: {
+    name: 'GenericError',
+    message: 'No error message provided'
+  }
 }
 
 export default Error
