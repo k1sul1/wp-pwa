25b3f66f804c30c2264c9ead348ebad4c1de62b6
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-17 17:41:04 +0200

    Remove warning to allow CI to complete

--- src/routes/RoutingError.js
+++ src/routes/RoutingError.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { defaultSidebar } from '../components/Sidebar'
-import LoginForm from '../components/LoginForm'
 
 const RoutingError = (props) => (
   <Layout sidebar={defaultSidebar} className="routing-error" {...props}>
