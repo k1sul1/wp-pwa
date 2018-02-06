2d9e7ff943cb8ec565327cd68769028bb56af257
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-02-06 14:55:13 +0200

    Fix return type

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -64,7 +64,7 @@ export const transformWPContent = (...args) => {
       </SyntaxHighlighter>
     )
   } else if (isCodechild(...args)) {
-    return false
+    return null
   }
 
   return undefined
